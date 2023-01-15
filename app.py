d1856a4224044d3b4b661c2f9d164445b8c7849f
1MPieces <elisehudson2014@gmail.com>
2023-01-14 20:19:19 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-# app.py
+# app.py Double inside
 
 import streamlit as st
+
 st.header('Tossing a Coin')
+number of trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button:
+    st.write(f'Running the experiment of {number_of_trials} trials.')
+
 st.write('It is not a functional application yet. Under construction.')
